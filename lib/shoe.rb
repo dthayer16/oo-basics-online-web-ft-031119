b33158998af465220df98bef4d39bdf79aa76c42
David Thayer <dthayer16@gmail.com>
2019-03-23 22:35:12 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,10 @@
 # Make your shoe class here!
 class shoe
-attr_accessor :brand, :color, :size, :material, :condition
+attr_accessor :color, :size, :material, :condition
+attr_reader :brand
+
+def initialize(brand)
+  @brand = brand
+end
+  
 end
